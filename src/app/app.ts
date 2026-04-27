39e072e780ea3a3ae6b6ff8c5c94a460977235e1
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-04-27 14:25:15 -0300

    feat(custom): Customize the Angular Material Theme.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatToolbarModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/parceiros/parceiros/parceiros.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-parceiros',
+  imports: [],
+  templateUrl: './parceiros.html',
+  styleUrl: './parceiros.scss',
+})
+export class Parceiros {}

668726444d6f9b75d680281f37a2dfab8ed08a1b
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-04-27 20:44:45 -0300

    feat(Material Table): add CSS to Material Tables and Creating a Material App Module

--- src/app/parceiros/parceiros.module.ts
+++ src/app/parceiros/parceiros.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
+import { MatTableModule } from '@angular/material/table';
 import { ParceirosRoutingModule } from './parceiros-routing.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ParceirosRoutingModule],
+  imports: [CommonModule, ParceirosRoutingModule,
+    MatTableModule
+  ],
 })
 export class ParceirosModule {}
---
+++ src/app/shared/app-material/app-material-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+
+@NgModule({
+  exports: [MatFormFieldModule, MatInputModule, MatTableModule,
+  MatCardModule],
+})
+export class AppMaterialModule {}

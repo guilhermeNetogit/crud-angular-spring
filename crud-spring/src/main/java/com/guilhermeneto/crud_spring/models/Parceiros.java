a8d174e2db6e83dc6a74d0611cb00526c8c45c97
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-05-05 14:19:16 -0300

    feat(validation): API validations with Java Bean Validation.

--- crud-spring/src/main/java/com/guilhermeneto/crud_spring/models/Parceiros.java
+++ crud-spring/src/main/java/com/guilhermeneto/crud_spring/models/Parceiros.java
@@ -2,12 +2,16 @@ package com.guilhermeneto.crud_spring.models;
 
 import java.math.BigDecimal;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,16 +24,22 @@ import lombok.NoArgsConstructor;
 
 public class Parceiros {
 
+    @NotNull
+    @Positive
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Positive
     @Column(nullable = false)
     private Integer position;
 
-    @Column(nullable = false, length = 40)
+    @NotNull
+    @Length(min = 2, max = 30)
+    @Column(nullable = false, length = 30)
     private String name;
 
+    @Positive
     @Column(nullable = true, length = 10, scale = 6)
     private BigDecimal weight;
 

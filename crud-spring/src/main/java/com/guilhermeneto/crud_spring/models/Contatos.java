310735746602201f2d02ee4c91be7a9cab6a4a7a
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-05-07 15:11:23 -0300

    feat(): add a two-way relationship between Partners and Contacts.

---
+++ crud-spring/src/main/java/com/guilhermeneto/crud_spring/models/Contatos.java
@@ -0,0 +1,36 @@
+package com.guilhermeneto.crud_spring.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TGFCONT")
+public class Contatos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codcontato;
+
+    @Column(nullable = false, length = 30)
+    private String nomecontato;
+
+    @Column(length = 20)
+    private String telefone;
+
+    private String email;
+
+    private String siteurl;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "codparc_id", nullable = false)
+    private Parceiros parceiros;
+}

a84325b2a0b62b92b269117be60cac8de14191c0
guilhermeNetogit <eng.guilhermeneto@hotmail.com>
2026-05-11 23:33:25 -0300

    feat(pagination): add pagination in Spring.

--- crud-spring/src/main/java/com/guilhermeneto/crud_spring/controllers/ParceirosController.java
+++ crud-spring/src/main/java/com/guilhermeneto/crud_spring/controllers/ParceirosController.java
@@ -1,7 +1,5 @@
 package com.guilhermeneto.crud_spring.controllers;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +8,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.guilhermeneto.crud_spring.dtos.ParceiroPageDto;
 import com.guilhermeneto.crud_spring.dtos.ParceiroRequestDto;
 import com.guilhermeneto.crud_spring.dtos.ParceiroResponseDto;
 import com.guilhermeneto.crud_spring.services.ParceirosService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 
 @RestController
 @RequestMapping("/api/parceiros")
@@ -32,8 +35,11 @@ public class ParceirosController {
     }
 
     @GetMapping()
-    public List<ParceiroResponseDto> getParceiros() {
-        return parceiroService.getParceiros();
+    public ParceiroPageDto getParceiros(
+        @RequestParam(defaultValue = "0") @PositiveOrZero int page, 
+        @RequestParam(defaultValue = "10") @Positive @Max(20) int pageSize
+    ) {
+        return parceiroService.getParceiros(page, pageSize);
     }
 
     @GetMapping("/{id}")
